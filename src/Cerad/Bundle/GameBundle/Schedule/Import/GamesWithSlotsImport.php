a465d039fab3f529afd0f1ac8e493dd54bffe53a
ahundiak <ahundiak@spike.mshome.net>
2013-09-17 17:07:08 -0500

    Import sample data

---
+++ src/Cerad/Bundle/GameBundle/Schedule/Import/GamesWithSlotsImport.php
@@ -0,0 +1,31 @@
+<?php
+namespace Cerad\Bundle\GameBundle\Schedule\Import;
+
+class GamesWithSlotsImport
+{
+    
+}
+/* =====================================
+ * <Detail 
+ * Note_Date="" Game_Note="No Note" 
+ * Fifth_Official="Empty" Fifth_Position="No Fifth Position" 
+ * Fourth_Official="Empty" Fourth_Position="No Fourth Position" 
+ * Third_Official="Empty" Third_Position="No Third Position" 
+ * Second_Official="Empty" Second_Position="No Second Position" 
+ * First_Official="Tom Lawson" First_Position="Referee" 
+ * Slots_Total="1" 
+ * Reporting_Official="" Report_Status="No Report" Report_Posted_Date="1900-01-01T00:00:00" 
+ * Game_Report_Comments="" 
+ * Status="Normal" 
+ * Total_Game_Fees="25.00" Bill_Amount="25.00" BillTo_Name="HISL" 
+ * Away_Score="" Away_Team="3-4_SJS-3" 
+ * Home_Score="" Home_Team="3-4_Rand_Blu" 
+ * Subsite="" Site="Randolph Drake Campus" 
+ * Level="3rd4th" Sport="HISL" 
+ * To_Date="2013-09-17T17:35:00" 
+ * From_Date="2013-09-17T16:45:00" 
+ * Game_LinkID="1091" 
+ * GameID="2068"
+ * />
+ */
+?>
---
+++ src/Cerad/Bundle/GameBundle/Schedule/Import/SlotsWithStatusImport.php
@@ -0,0 +1,21 @@
+<?php
+namespace Cerad\Bundle\GameBundle\Schedule\Import;
+
+class SlotsWithStatusImport
+{
+    
+}
+/* =====================================
+ * <Detail 
+ * Status="Accepted" 
+ * AwayTeams="5-6_UC" HomeTeams="5-6_Excal" 
+ * SiteName="Brahan Spring" SportName="HISL" 
+ * LevelName="5th6th" 
+ * FromDate="2013-09-17T17:00:00" 
+ * LastName="Haynes" 
+ * FirstName="Scott" 
+ * PositionName="Referee" 
+ * GameID="2065"
+ * />
+ */
+?>
