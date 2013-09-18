54fdbe7d6ef8072095ca6507a4bf5259b57d88c2
ahundiak <ahundiak@spike.mshome.net>
2013-09-18 11:14:33 -0500

    XML Import loop working

---
+++ src/Cerad/Bundle/GameBundle/Command/ScheduleImportCommand.php
@@ -0,0 +1,96 @@
+<?php
+namespace Cerad\Bundle\GameBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+//  Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ScheduleImportCommand extends ContainerAwareCommand
+{
+    protected $commandName = 'command';
+    protected $commandDesc = 'Command Description';
+    
+    protected function configure()
+    {
+        $this
+            ->setName       ('cerad:schedule:import')
+            ->setDescription('Schedule Import')
+            ->addArgument   ('importFile', InputArgument::REQUIRED, 'Import File')
+            ->addArgument   ('truncate',   InputArgument::OPTIONAL, 'Truncate')
+        ;
+    }
+    protected function getService  ($id)   { return $this->getContainer()->get($id); }
+    protected function getParameter($name) { return $this->getContainer()->getParameter($name); }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $importFile = $input->getArgument('importFile');
+        $truncate   = $input->getArgument('truncate');
+        
+        if ($truncate) $truncate = true;
+        
+        $this->loadFile($importFile,$truncate);
+
+    }
+    /* =========================================================
+     * gws/TEST_Fall2013_GamesWithSlots_20130917.xml
+     * 
+     * pathInfo
+     *  [dirname]   => gws
+     *  [basename]  => TEST_Fall2013_GamesWithSlots_20130917.xml
+     *  [extension] => xml
+     *  [filename]  => TEST_Fall2013_GamesWithSlots_20130917
+     * 
+     * parts
+     * [0] => domain => TEST
+     * [1] => season => Fall2013
+     * [2] => format => GamesWithSlots
+     * [3] => suffix => 20130917
+     */
+    protected function loadFile($filePath, $truncate)
+    {   
+        // Add in default directory and make sure it exists
+        $datax  = $this->getParameter('cerad_datax');
+        
+        $filePath = $datax . $filePath;
+        
+        if (!file_exists($filePath)) 
+        {
+            echo sprintf("*** File does not exist: %s\n",$filePath);
+            return;
+        }
+        
+        $pathInfo = pathinfo($filePath);
+        
+        $parts = explode('_',$pathInfo['filename']);
+        
+        if (count($parts) < 4)
+        {
+            echo sprintf("*** %d File Name Format: DOMAIN_Season_Format_Date\n",count($parts));
+            return;
+        }
+        
+        $paramsx = array();
+        
+        $paramsx['filepath'] = $filePath;
+        
+        $params = array_merge($paramsx,$pathInfo);
+        
+        $params['sport']  = 'Soccer';
+        $params['domain'] = $parts[0];
+        $params['season'] = $parts[1];
+        $params['format'] = $parts[2];
+        $params['suffix'] = $parts[3];
+        
+        $importServiceId = sprintf('cerad_game.schedule_%s.import',$params['format']);
+        $importService = $this->getService($importServiceId);
+        
+        $results = $importService->import($params);
+        
+        print_r($results);
+        
+    }
+}
+?>
--- src/Cerad/Bundle/GameBundle/Schedule/Import/GamesWithSlotsImport.php
+++ src/Cerad/Bundle/GameBundle/Schedule/Import/GamesWithSlotsImport.php
@@ -1,9 +1,138 @@
 <?php
 namespace Cerad\Bundle\GameBundle\Schedule\Import;
 
-class GamesWithSlotsImport
+class Results
 {
     
+}
+class GamesWithSlotsImport
+{
+    protected $results;
+    
+    protected $sport;
+    protected $domain;
+    protected $season;
+    protected $nums;
+    
+    protected function processRow($row)
+    {
+        // Process the game number
+        $num = (integer)$row['num'];
+        
+        // Probably toss an exception
+        if (!$num) return;
+        
+        // Really should not happem but does
+        if (isset($this->nums[$num])) return;
+        $this->nums[$num] = true;
+        
+        echo sprintf("Row %d %s %s %s %s\n",$num,$row['season'],$row['domain'],$row['domainSub'],$row['level']);
+        
+        return;
+    }
+    public function import($params)
+    {
+        // Save some
+        $this->sport  = $params['sport'];
+        $this->domain = $params['domain'];
+        $this->season = $params['season'];
+        $this->nums = array();
+        
+        // Setup results collector
+        $this->results = $results = new Results();
+        $results->basename = $params['basename'];
+        $results->totalGamesCount = 0;
+        
+        // Must be a report file
+        $reader = new \XMLReader();
+        $reader->open($params['filepath'],null,LIBXML_COMPACT | LIBXML_NOWARNING);
+        
+       // Position to Report node
+        if (!$reader->next('Report')) 
+        {
+            $results->message = '*** Not a Report file';
+            $reader->close();
+            return $results;
+        }
+        // Verify report type
+        $reportType = $reader->getAttribute('Name');
+        if ($reportType != 'Games with Slots')
+        {
+            $results->message = '*** Unexpected report type: ' . $reportType;
+            $reader->close();
+            return $results;
+        }
+        // Kind of screw but oh well
+        while ($reader->read() && $reader->name !== 'Detail');
+        
+        $map = $this->map;
+        
+        // Loop
+        while($reader->name == 'Detail')
+        {
+            $row = array();
+            $row['sport']  = $params['sport'];
+            $row['domain'] = $params['domain'];
+            $row['season'] = $params['season'];
+        
+            foreach($this->map as $key => $attr)
+            {
+                $row[$key] = $reader->getAttribute($attr);
+            }
+            $results->totalGamesCount++;
+            
+            $this->processRow($row);
+
+            // On to the next one
+            $reader->next('Detail');
+        }        
+        // Done
+        $reader->close();
+        return $results;
+    }
+    protected $map = array
+    (
+        'num'           => 'GameID',
+        'dtBeg'         => 'From_Date',    // 2013-03-08T16:30:00
+        'dtEnd'         => 'To_Date',
+        'domainSub'     => 'Sport',        // AHSAA
+        'level'         => 'Level',        // MS-B
+        'site'          => 'Site',
+        'siteSub'       => 'Subsite',
+        'homeTeamName'  => 'Home_Team',
+        'homeTeamScore' => 'Home_Score',
+        'awayTeamName'  => 'Away_Team',
+        'awayTeamScore' => 'Away_Score',
+        
+        'status'        => 'Status',
+        
+        'officialSlots' => 'Slots_Total',
+        
+        'officialRole1' => 'First_Position',  // Referee 
+        'officialRole2' => 'Second_Position', // AR1 (or possibly dual?
+        'officialRole3' => 'Third_Position',  // AR2
+        'officialRole4' => 'Fourth_Position', // 'No Fourth Position'
+        'officialRole5' => 'Fifth_Position',  // 'No Fifth Position' 
+        
+        'officialName1' => 'First_Official', 
+        'officialName2' => 'Second_Official', 
+        'officialName3' => 'Third_Official', 
+        'officialName4' => 'Fourth_Official',  // 'Empty'
+        'officialName5' => 'Fifth_Official',   // 'Empty'
+        
+        'billTo'        => 'BillTo_Name',
+        'billAmount'    => 'Bill_Amount',     // 100.00
+        'billFees'      => 'Total_Game_Fees', //  37.00 ?
+        
+        'gameNote'      => 'Game_Note',    // 'No Note'
+        'gameNoteDate'  => 'Note_Date=',   //  Blank
+        
+        'gameReportComments' => 'Game_Report_Comments',
+        'gameReportDateTime' => 'Report_Posted_Date',   // 1900-01-01T00:00:00
+        'gameReportStatus'   => 'Report_Status',        // 'No Report'
+        'gameReportOfficial' => 'Reporting_Official',
+        
+    );
 }
 /* =====================================
  * <Detail 
